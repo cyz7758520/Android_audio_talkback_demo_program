6b3664e7ca61e1676c2948261f654bff5245f4ef
HeavenTao <280604597@qq.com>
2020-03-16 14:30:56 +0800

    COMMIT

---
+++ app/src/main/java/HeavenTao/Data/HTShort.java
@@ -0,0 +1,17 @@
+package HeavenTao.Data;
+
+public class HTShort
+{
+    public short m_Val;
+
+    //构造函数。
+    public HTShort()
+    {
+        m_Val = 0;
+    }
+
+    public HTShort( short Val )
+    {
+        m_Val = Val;
+    }
+}
